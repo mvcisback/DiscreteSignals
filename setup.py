819057d43af982cd2817098169890e33cc0d45f9
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-09-22 10:22:58 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='discrete-signals',
-    version='0.0.1',
+    version='0.2.0',
     description='TODO',
     url='http://github.com/mvcisback/DiscreteSignals',
     author='Marcell Vazquez-Chanlatte',
