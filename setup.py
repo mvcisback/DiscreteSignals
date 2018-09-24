42a40c61fae2e538d9706af48f9f678a57f6169e
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-09-24 01:09:51 -0700

    make attrs at least version 18.1.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ DESC = 'A domain specific language for modeling and manipulating discrete time s
 
 setup(
     name='discrete-signals',
-    version='0.7.2',
+    version='0.7.3',
     description=DESC,
     url='http://github.com/mvcisback/DiscreteSignals',
     author='Marcell Vazquez-Chanlatte',
     author_email='marcell.vc@eecs.berkeley.edu',
     license='MIT',
     install_requires=[
-        'attrs>=18',
+        'attrs>=18.1.0',
         'funcy',
         'sortedcontainers',
     ],
